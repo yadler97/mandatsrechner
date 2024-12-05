accd549748c996988f9bf9cc85d4d29397205643
Yannick Adler <yannick.adler@yahoo.de>
2024-12-05 20:07:55 +0100

    shorten select text

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -26,8 +26,8 @@
         <option value="{base}/landtagswahlSteiermark2024">Landtagswahl Steiermark 2024</option>
         <option value="{base}/landtagswahlBurgenland2025">Landtagswahl Burgenland 2025</option>
         <option value="{base}/bundestagswahl2025">Bundestagswahl 2025</option>
-        <option value="{base}/landtagswahlWien2025" disabled>Landtags- und Gemeinderatswahl Wien 2025</option>
-        <option value="{base}/bezirksvertretungswahlenWien2025" disabled>Bezirksvertretungswahlen Wien 2025</option>
+        <option value="{base}/landtagswahlWien2025" disabled>Landtagswahl Wien 2025</option>
+        <option value="{base}/bezirkswahlenWien2025" disabled>Bezirkswahlen Wien 2025</option>
 
         <option value="{base}/spielwiese">Spielwiese</option>
     </select>
