0b4156d2b900e260ae4b2635193a992d9a41e96f
Yannick Adler <yannick.adler@yahoo.de>
2026-03-25 00:54:23 +0100

    build fix

--- src/routes/(elections)/[election]/+page.js
+++ src/routes/(elections)/[election]/+page.js
@@ -11,3 +11,15 @@ export async function load({ params }) {
         throw error(404, 'Election not found');
     }
 }
+
+export function entries() {
+    const modules = import.meta.glob('/src/lib/elections/*.js');
+
+    return Object.keys(modules).map((path) => {
+        return {
+            election: (path.split('/').pop() ?? '').replace('.js', '')
+        };
+    });
+}
+
+export const prerender = true;
