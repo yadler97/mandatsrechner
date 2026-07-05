f9401016c34b492511df6bd876e6c4bb93cbb552
Yannick Adler <yannick.adler@yahoo.de>
2026-07-05 04:19:17 +0200

    new version

---
+++ _app/immutable/entry/start.BeFM9SLf.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/DBwJCKBk.js";import{y as t}from"../chunks/DzRMrU2N.js";export{t as load_css,r as start};
--- _app/immutable/entry/start.CAXffI8S.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"../chunks/C1VpgD6f.js";import{y as t}from"../chunks/CRVfV6br.js";export{t as load_css,r as start};
