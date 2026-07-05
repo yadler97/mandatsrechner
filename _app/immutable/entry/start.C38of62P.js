130bc5c46de56f30b5c33a13ad8db3dc320af1db
Yannick Adler <yannick.adler@yahoo.de>
2026-07-05 05:02:13 +0200

    new version

---
+++ _app/immutable/entry/start.C38of62P.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/jzsUR3yA.js";import{y as t}from"../chunks/Dog4W7bc.js";export{t as load_css,r as start};
--- _app/immutable/entry/start.arTVt2Mc.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"../chunks/UU5cWhf3.js";import{y as t}from"../chunks/C8BUn-lp.js";export{t as load_css,r as start};
